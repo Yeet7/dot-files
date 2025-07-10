89a05fb7ca13b7aadea0de44f2d3fa61826e7b93
Yeet7 <51092112+Yeet7@users.noreply.github.com>
2025-07-10 01:50:34 -0400

    Init

---
+++ polybar/launch.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+# Create env variable for polybar CPU temp.
+for i in /sys/class/hwmon/hwmon*/temp*_input; do 
+    if [ "$(<$(dirname $i)/name): $(cat ${i%_*}_label 2>/dev/null || echo $(basename ${i%_*}))" = "coretemp: temp1_input" ]; then
+        export HWMON_PATH="$i"
+    fi
+done
+
+killall -q polybar
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch both bars.
+polybar --reload --quiet top -c ~/.config/polybar/config.ini &
+polybar --reload --quiet bottom -c ~/.config/polybar/config.ini &
---
+++ polybar/scripts/ufw.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if pgrep -x steam > /dev/null 
+then
+    echo "%{F#9ece6a}󱨑 "
+else
+    echo "%{F#db4b4b}󱨑 "
+fi
+
+
